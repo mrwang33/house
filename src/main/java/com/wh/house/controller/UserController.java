2ebab1c0f80a4fda61874bdc35e81e07cd09f696
wang33 <wang1386528@gmail.com>
2018-03-28 17:30:32 +0800

    project init

---
+++ src/main/java/com/wh/house/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.wh.house.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @RequestMapping(value = "/user",method = RequestMethod.GET)
+    public String getUser() {
+        return "username:wang";
+    }
+}
