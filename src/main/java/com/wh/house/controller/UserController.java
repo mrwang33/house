5ced1d6994b0448b655603d075af2056645d9906
wang33 <wang1386528@gmail.com>
2018-03-29 15:20:05 +0800

    freemark config

--- src/main/java/com/wh/house/controller/UserController.java
+++ src/main/java/com/wh/house/controller/UserController.java
@@ -2,21 +2,25 @@ package com.wh.house.controller;
 
 import com.wh.house.entity.User;
 import com.wh.house.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 
-@RestController
+@Controller
 public class UserController {
     @Resource
     private UserService userService;
 
 
     @RequestMapping(value = "/user/{userId}",method = RequestMethod.GET)
-    public User getUser(@PathVariable("userId") Integer userId) {
-        return userService.getById(userId);
+    public String getUser(@PathVariable("userId") Integer userId, ModelMap modelMap) {
+
+        User byId = userService.getById(userId);
+        modelMap.addAttribute("user",byId);
+        return "user";
     }
 }
---
+++ src/main/resources/templates/user.ftl
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <p>hello,${user.username}</p>
+</head>
+<body>
+
+</body>
+</html>
