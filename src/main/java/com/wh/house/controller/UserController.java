841155fa3140abe978e2ac01bdcb9ec2e80670b0
wang33 <wang1386528@gmail.com>
2018-03-29 11:53:16 +0800

    mybatis config

--- src/main/java/com/wh/house/controller/UserController.java
+++ src/main/java/com/wh/house/controller/UserController.java
@@ -1,14 +1,22 @@
 package com.wh.house.controller;
 
+import com.wh.house.entity.User;
+import com.wh.house.service.UserService;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+
 @RestController
 public class UserController {
+    @Resource
+    private UserService userService;
 
-    @RequestMapping(value = "/user",method = RequestMethod.GET)
-    public String getUser() {
-        return "username:wang";
+
+    @RequestMapping(value = "/user/{userId}",method = RequestMethod.GET)
+    public User getUser(@PathVariable("userId") Integer userId) {
+        return userService.getById(userId);
     }
 }
---
+++ src/main/java/com/wh/house/service/UserService.java
@@ -0,0 +1,7 @@
+package com.wh.house.service;
+
+import com.wh.house.entity.User;
+
+public interface UserService extends BaseService<User> {
+
+}
